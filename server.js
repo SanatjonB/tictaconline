42e819dd5f91cadd3866708dfb4957d4ce4d6ef3
Sanatjon Burkhanov <sburhanov1977@gmail.com>
2024-05-28 21:44:24 -0400

    init3

--- server.js
+++ server.js
@@ -2,19 +2,23 @@ const express = require("express");
 const http = require("http");
 const cors = require("cors");
 const socketIO = require("socket.io");
-const { log } = require("util");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIO(server, {
   cors: {
     origin: "https://tic-tac-toe-front-end-one.vercel.app",
-    method: ["GET", "POST"],
+    methods: ["GET", "POST"],
   },
 });
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 app.use(cors());
+
+let game = {
+  board: Array(9).fill(null),
+  currentPlayer: "X",
+};
 
 app.get("/game", (req, res) => {
   res.status(200).send("TIC Tac Toe Game Server");
@@ -23,17 +27,23 @@ app.get("/game", (req, res) => {
 io.on("connection", (socket) => {
   console.log("A user connected");
 
+  socket.emit("gameReset", game);
+
   socket.on("makeMove", (data) => {
+    game = data.updatedGame;
     io.emit("moveMade", data);
   });
+
   socket.on("resetGame", (newGame) => {
+    game = newGame;
     io.emit("gameReset", newGame);
   });
+
   socket.on("disconnect", () => {
-    console.log("UserDisconnected");
+    console.log("User disconnected");
   });
 });
 
 server.listen(port, () => {
-  console.log(`server running at http://localhost:${port}`);
+  console.log(`Server running at http://localhost:${port}`);
 });
